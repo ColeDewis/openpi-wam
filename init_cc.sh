083c124cc6e2f24257263e4bf8ef8e8eb1b57d78
Sergey Khlynovskiy <khlynovs@ualberta.ca>
2026-05-24 12:23:03 -0600

    test job

---
+++ init_cc.sh
@@ -0,0 +1,7 @@
+module load python/3.11
+module load cuda/12.2
+module load cudnn/8.9.5.29    
+module load cmake/3.31.0
+module load opencv/4.11.0
+module load mujoco/3.3.0 
+module load gcc arrow/23.0.1
---
+++ job_scripts/test_job.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=test_wam
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --output=%x-%j.out
+
+module purge
+module load python/3.11
+module load cuda/12.2
+module load cudnn/8.9.5.29
+module load cmake/3.31.0
+module load opencv/4.11.0
+module load mujoco/3.3.0
+module load gcc arrow/23.0.1
+
+echo "move venv"
+cp $path/venv311.tar $SLURM_TMPDIR/
+cd $SLURM_TMPDIR
+tar -xf my_venv.tar
+
+source path/to/your/venv/bin/activate
+
+echo "move data"
+cp $path/hf_dataset.tar $SLURM_TMPDIR/
+mkdir -p $SLURM_TMPDIR/huggingface
+tar -xf hf_dataset.tar -C $SLURM_TMPDIR/huggingface
+
+export HF_HOME=$SLURM_TMPDIR/huggingface
+export HF_DATASETS_CACHE=$SLURM_TMPDIR/huggingface/datasets
+export HF_DATASETS_OFFLINE=1
+
+echo "starting"
+python3 $path/scripts/train.py haptic_wam --exp-name=haptic_wam_test --overwrite
