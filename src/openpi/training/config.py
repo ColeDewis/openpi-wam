9bee7ee98ef556d286f3ade888ab9722ebb350c4
Sergey Khlynovskiy <khlynovs@ualberta.ca>
2026-05-24 00:34:23 -0600

    new hf repo

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -805,7 +805,7 @@ _CONFIGS = [
         model=pi0_config.Pi0Config(pi05=True, action_horizon=10, discrete_state_input=False),
         data=LeRobotLiberoDataConfig(
             # repo_id="physical-intelligence/libero",
-            repo_id="/home/serg/serg/openpi-wam/wam_teleop_dataset",
+            repo_id="Breakdancingbear/wam_teleop_dataset",
             base_config=DataConfig(prompt_from_task=True),
             extra_delta_transform=False,
         ),
