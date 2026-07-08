ebe41346902e6a1e355a0ed772deaebb821582ee
Sergey Khlynovskiy <khlynovs@ualberta.ca>
2026-07-07 23:23:41 -0600

    actually keep the checkpoints

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -840,6 +840,7 @@ _CONFIGS = [
         ),
         optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
         ema_decay=0.999,
+        keep_period=1000,
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
         num_train_steps=15_000,
     ),
