7c1261cd1e9d1109986215212dabe9d3c3e9e36f
Dylan Miller <djm2@ualberta.ca>
2026-07-25 07:08:06 -0600

    cleanup infer test script

--- wam/inference/infer_dataset.py
+++ wam/inference/infer_dataset.py
@@ -1,28 +1,23 @@
 import dataclasses
 from pathlib import Path
 
-import lerobot.common.datasets.lerobot_dataset as lerobot_dataset
 import matplotlib.pyplot as plt
 
 from openpi.policies import policy_config as _policy_config
 from openpi.training import config as _config
-from openpi.training.data_loader import TransformedDataset, create_torch_dataset
+from openpi.training.data_loader import TransformedDataset
+from openpi.training.data_loader import create_torch_dataset
 
 if __name__ == "__main__":
-
     config = _config.get_config("haptic_wam_pi05")
     config = dataclasses.replace(config, batch_size=1)
-    checkpoint_dir = Path(
-        "/mnt/10tb/dyanmiller/openpi-wam/checkpoints/haptic_wam_pi05/haptic_wam_pi05/29999"
-    )
+    checkpoint_dir = Path("/mnt/10tb/dylanmiller/openpi-wam/checkpoints/haptic_wam_pi05/haptic_wam_pi05/29999")
 
     # Create a trained policy.
     policy = _policy_config.create_trained_policy(config, checkpoint_dir)
 
     data_config = config.data.create(config.assets_dirs, config.model)
-    dataset = create_torch_dataset(
-        data_config, config.model.action_horizon, config.model
-    )
+    dataset = create_torch_dataset(data_config, config.model.action_horizon, config.model)
 
     dataset = TransformedDataset(
         dataset,
@@ -30,7 +25,6 @@ if __name__ == "__main__":
             *data_config.repack_transforms.inputs,
         ],
     )
-
 
     i = 5000
     while i < len(dataset):
@@ -58,4 +52,3 @@ if __name__ == "__main__":
         plt.show()
 
         i += 5
-
